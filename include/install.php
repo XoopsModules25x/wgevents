37a9a745a589eb481a30f2681ece746bac2e6c1b
NB-GABOR02\Goffy <gottfried.gabor@gmail.com>
2026-05-24 11:59:54 +0200

    remove silent data killer reinstall or recovery

--- include/install.php
+++ include/install.php
@@ -40,13 +40,6 @@ function xoops_module_pre_install_wgevents(\XoopsModule $module)
     // check for minimum PHP version
     $phpSuccess = $utility::checkVerPhp($module);
 
-    if ($xoopsSuccess && $phpSuccess) {
-        $moduleTables = &$module->getInfo('tables');
-        foreach ($moduleTables as $table) {
-            $GLOBALS['xoopsDB']->queryF('DROP TABLE IF EXISTS ' . $GLOBALS['xoopsDB']->prefix($table) . ';');
-        }
-    }
-
     return $xoopsSuccess && $phpSuccess;
 }
 
